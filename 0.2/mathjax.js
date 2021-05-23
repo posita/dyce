f811a6b54a8ba415b00aede2fa532d9748fca8e5
Matt Bogosian <matt@bogosian.net>
2021-05-23 17:56:53 -0600

    Deployed 59f1663 to 0.2 with MkDocs 1.1.2 and mike 1.0.0

---
+++ 0.2/mathjax.js
@@ -0,0 +1,17 @@
+// See https://squidfunk.github.io/mkdocs-material/reference/mathjax/
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise()
+})
